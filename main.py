fe187630e731126bb6a69cfd1fcad6c989b56e77
S4NYAKABAN <sani4kapro@knu.ua>
2026-04-08 14:03:55 +0300

    Feat: add count_words method

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ class TextAnalyzer:
     def read_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             return f.read()
+
+    def count_words(self, text):
+        # Розділювачі: кома, пробіл, двокрапка, крапка з комою
+        words = re.split(r'[ ,:;]+', text)
+        return len([w for w in words if w.strip()])
